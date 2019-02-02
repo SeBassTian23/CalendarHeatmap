294a9309409dd73342fc1e3c9094ab1e75005033
Sebastian <sebastian.kuhlgert@gmail.com>
2019-02-01 23:42:06 -0500

    Removing phantomjs for test, karma config fixed
    No more development on phantomjs at this point…

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,14 +83,14 @@ module.exports = function (grunt) {
 				configFile: "karma.conf.js",
 				background: true,
 				singleRun: false,
-				browsers: ["PhantomJS", "Firefox"]
+				browsers: ["Chrome"]
 			},
 
 			//continuous integration mode: run tests once in PhantomJS browser.
 			travis: {
 				configFile: "karma.conf.js",
 				singleRun: true,
-				browsers: ["PhantomJS"]
+				browsers: ["Chrome"]
 			}
 		},
 
