198b283f3180d966c0d79b54d1370fdbc594497a
Sebastian <sebastian.kuhlgert@gmail.com>
2021-03-08 16:19:29 -0500

    update wrapper

--- src/jquery.CalendarHeatmap.js
+++ src/jquery.CalendarHeatmap.js
@@ -1,4 +1,4 @@
-;( function( $ ) {
+( function( $ ) {
 
     "use strict";
 
@@ -484,12 +484,60 @@
         // A really lightweight plugin wrapper around the constructor,
         // preventing against multiple instantiations
         $.fn[ pluginName ] = function( data, options ) {
-            return this.each( function() {
-                if ( !$.data( this, "plugin_" + pluginName ) ) {
-                    $.data( this, "plugin_" +
-                        pluginName, new Plugin( this, data, options ) );
-                }
-            } );
+            var args = arguments;
+
+            // Is the first parameter an object (options), or was omitted,
+            // instantiate a new instance of the plugin.
+            // if ( data === undefined || typeof data === "object" ) {
+            if ( Array.isArray( data ) ) {
+                return this.each( function() {
+
+                    // Only allow the plugin to be instantiated once,
+                    // so we check that the element has no plugin instantiation yet
+                    if ( !$.data( this, "plugin_" + pluginName ) ) {
+
+                        // if it has no instance, create a new one,
+                        // pass options to our plugin constructor,
+                        // and store the plugin instance
+                        // in the elements jQuery data object.
+                        $.data( this, "plugin_" + pluginName, new Plugin( this, data, options ) );
+                    }
+                } );
+
+            // If the first parameter is a string and it doesn't start
+            // with an underscore or "contains" the `init`-function,
+            // treat this as a call to a public method.
+            } else if ( typeof data === "string" && data[ 0 ] !== "_" && data !== "init" ) {
+
+                // Cache the method call
+                // to make it possible
+                // to return a value
+                var returns;
+
+                this.each( function() {
+                    var instance = $.data( this, "plugin_" + pluginName );
+
+                    // Tests that there's already a plugin-instance
+                    // and checks that the requested public method exists
+                    if ( instance instanceof Plugin && typeof instance[ data ] === "function" ) {
+
+                        // Call the method of our plugin instance,
+                        // and pass it the supplied arguments.
+                        returns = instance[ data ].apply( instance,
+                            Array.prototype.slice.call( args, 1 ) );
+                    }
+
+                    // Allow instances to be destroyed via the 'destroy' method
+                    if ( data === "destroy" ) {
+                        $( this ).removeData();
+                    }
+                } );
+
+                // If the earlier cached method
+                // gives a value back return the value,
+                // otherwise return this to preserve chainability.
+                return returns !== undefined ? returns : this;
+            }
         };
 
 } )( jQuery );
